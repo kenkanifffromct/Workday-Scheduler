d1aea39ad6112bed0da7776a526085dd0162d919
Kenneth Keller <kenkanifffromct21@gmail.com>
2020-12-21 19:05:21 -0500

    Updating html, css, script, adding favicon & moment.js docs

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready(function() {
+    var save = document.querySelector('.fas fa-save');
+    $(save).on("click", function() {
+        alert("You've clicked 'save'!");
+      });
+    })
